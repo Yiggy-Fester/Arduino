6db3032c3d4ad72a15b6073f4feb095ef5c3651c
Yiggi-Fester@git.com <marko.miljevic@protonmail.com>
2022-01-10 12:14:23 +0100

    Fixed thing in new.pm where sometimes it wouldn't compile or upload .ino files to the board

--- new.pm
+++ new.pm
@@ -5,19 +5,19 @@ use Tk;
 
 sub light_on
 {
-  my $var = "cd sketchbook && cd light_on && arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno light_on.ino && cd";
+  my $var = "cd sketchbook && cd light_on && arduino-cli upload -p /dev/ttyACM0 -b arduino:avr:uno light_on.ino && cd";
   system ($var);
 }
 sub light_off
 {
-  my $var1 = "cd sketchbook && cd kill && arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno kill.ino && cd";
+  my $var1 = "cd sketchbook && cd kill && arduino-cli upload -p /dev/ttyACM0 -b arduino:avr:uno kill.ino && cd";
   system ($var1);
 }
 
 #compiling arduino sketches
-my $comp1="cd sketchbook && cd light_on && arduino-cli compile --fqbn arduino:avr:uno light_on.ino && cd";
+my $comp1="cd sketchbook && cd light_on && arduino-cli compile -b arduino:avr:uno light_on.ino && cd";
 system ($comp1);
-my $comp2="cd sketchbook && cd kill && arduino-cli compile --fqbn arduno:avr:uno kill.ino && cd";
+my $comp2="cd sketchbook && cd kill && arduino-cli compile -b arduno:avr:uno kill.ino && cd";
 system ($comp2);
 
 #Creating UI
