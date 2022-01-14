10338e8b8c25fb161c93ea332c976e16dbf5cbe3
Yiggi-Fester@git.com <marko.miljevic@protonmail.com>
2022-01-14 16:17:12 +0100

    Made it so that one button turns the light both on and off.

--- new.pm
+++ new.pm
@@ -1,29 +1,36 @@
 use Modern::Perl;
 use Tk; 
 
-#Creating functiuons
+my $light_count_1 = 1;
 
-sub light_on
+
+#Button Functions
+
+sub light_ctrl_1
 {
-  my $var = "cd sketchbook && cd light_on && arduino-cli upload -p /dev/ttyACM0 -b arduino:avr:uno light_on.ino && cd";
-  system ($var);
-}
-sub light_off
-{
-  my $var1 = "cd sketchbook && cd kill && arduino-cli upload -p /dev/ttyACM0 -b arduino:avr:uno kill.ino && cd";
-  system ($var1);
+  if ($light_count_1 == 1)
+  {
+    my $var = "cd sketchbook && cd light_on && arduino-cli upload -p /dev/ttyACM0 -b arduino:avr:uno light_on.ino && cd .. && cd ..";
+    system ($var);
+    $light_count_1 = 0
+  }
+  else
+  {
+    my $var1 = "cd sketchbook && cd kill && arduino-cli upload -p /dev/ttyACM0 -b arduino:avr:uno kill.ino && cd .. && cd ..";
+    system ($var1);
+    $light_count_1 = 1
+  }
 }
 
 #compiling arduino sketches
-my $comp1="cd sketchbook && cd light_on && arduino-cli compile -b arduino:avr:uno light_on.ino && cd";
+my $comp1="cd sketchbook && cd light_on && arduino-cli compile -b arduino:avr:uno light_on.ino && cd .. && cd ..";
 system ($comp1);
-my $comp2="cd sketchbook && cd kill && arduino-cli compile -b arduno:avr:uno kill.ino && cd";
+my $comp2="cd sketchbook && cd kill && arduino-cli compile -b arduino:avr:uno kill.ino && cd .. && cd ..";
 system ($comp2);
 
 #Creating UI
 
 my $main = new MainWindow;
 $main->Label(-text => 'Hello, world!')->pack;
-$main->Button(-text => 'Light on!', -command => sub{light_on})->pack;
-$main->Button(-text => 'Light off!', -command => sub{light_off})->pack;
+$main->Button(-text => 'Light!', -command => sub{light_ctrl_1})->pack;
 MainLoop;
